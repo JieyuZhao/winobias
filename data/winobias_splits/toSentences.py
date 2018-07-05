6a3bbb1c6d537afbd67205748cf662af0482322f
Jieyu Zhao <jz4fu@virginia.edu>
2018-07-05 17:41:48 -0400

    to sentence

---
+++ data/winobias_splits/toSentences.py
@@ -0,0 +1,18 @@
+import sys
+import re
+
+'''change the annotated sentences to plain text'''
+annos = ['[',']', '(', ')']
+lines = open(sys.argv[1]).readlines()
+# lines = ["300 [The president] was sued because [he] was embezzling funds."]
+sents = []
+annotations = []
+# lines=['881 [The police] saved the girl from the criminals because [he] upholds the peace.']
+for line in lines:
+    words = re.sub('^\s*[0-9]+\s*', '', line)
+    x = re.sub('[\[\]\(\)\{\}]',"", words)
+    sents.append(x)
+
+for i in range(len(sents)):
+    with open(sys.argv[2]+str(i), 'w') as f:
+        f.write(sents[i])
